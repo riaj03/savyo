9fdefae676d0431d7f5dd183d8beb2d0a122682c
riaj03 <riaj.csp@gmail.com>
2025-09-21 19:48:41 +0600

    LLLL

--- frontend/src/pages/CreateDeal.tsx
+++ frontend/src/pages/CreateDeal.tsx
@@ -245,7 +245,7 @@ const CreateDeal = () => {
                   htmlFor="expiryDate"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  Expiry Date
+                  Expiry Date and Time
                 </label>
                 <div className="mt-1 relative rounded-md shadow-sm">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
